8f143dbdf229c0750c9c562993b13aa11724c2f7
Rogerio Messias <rogerio.messias@outlook.com>
2025-10-08 21:52:51 -0300

    finalizando atividades

---
+++ exercicios-individuais/projetos/viagem/viagem.js
@@ -0,0 +1,13 @@
+const distanciaKm = document.querySelector("#distancia-km");
+const consumoKm = document.querySelector("#consumo-km");
+const precoCombustivel = document.querySelector("#preco-combustivel");
+const numeroViajantes = document.querySelector("#numero-viajantes");
+
+const custoTotal = document.querySelector("#custo-total");
+const custoPorPessoa = document.querySelector("#custo-por-pessoa");
+
+
+function calcularDespesas(){
+
+    
+}
